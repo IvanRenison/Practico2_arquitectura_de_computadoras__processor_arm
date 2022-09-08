00c00b73af014ea1837e93b4d73c5e178f292abf
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-09-08 17:28:24 -0300

    Corregido 2a

--- Códigos de test LEGv8/2A.s
+++ Códigos de test LEGv8/2A.s
@@ -1,12 +1,12 @@
-// N = 30
+// N = 29
 
-	ADD x2, xzr, xzr
-loop:
+	ADD x2, xzr, xzr // x2 = 0
+loop: // while x0 != 30
+	SUB x3, x0, x30
+	CBZ x3, end
 	STUR x0, [x2, #0]
 	ADD x0, x1, x0
 	ADD x2, x2, x8
-	SUB x3, x0, x30
-	CBZ x3, end
 	CBZ xzr, loop
 end:
 
