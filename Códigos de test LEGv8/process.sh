fdc7811fc5550d806d1ba40ffeba71b3ec28e81d
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-09-09 09:33:40 -0300

    Mejorado el process.sh

--- Códigos de test LEGv8/process.sh
+++ Códigos de test LEGv8/process.sh
@@ -4,6 +4,6 @@ FILE=$1
 
 LONG=$(cat $FILE.list | grep -oP '\t[0-9a-f]{8}' | wc -l)
 
-cat $FILE.list | grep -oP '\t[0-9a-f]{8}' | awk -v awkvar="$LONG" '{if (NR==1) {print "ROM [0:" awkvar-1 "] =\x27{32\x27h" $1 ","} else { if (NR==awkvar){print "32\x27h" $1 "};"} else {print "32\x27h" $1 ","}}}'
+cat $FILE.list | grep -oP '\t[0-9a-f]{8}' | awk -v awkvar="$LONG" '{if (NR==1) {print "		rom [0:" awkvar-1 "] = \x27{32\x27h" $1 ","} else { if (NR==awkvar){print "			32\x27h" $1 "\n		};"} else {print "			32\x27h" $1 ","}}}'
 
 
