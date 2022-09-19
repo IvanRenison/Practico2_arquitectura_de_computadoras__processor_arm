15e0e439320e7fc9e975d52fb17c225521f9f486
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-09-19 12:46:13 -0300

    Emprlijados los módulos de los profes

--- aludec.sv
+++ aludec.sv
@@ -1,15 +1,24 @@
 // ALU CONTROL DECODER
 
-module aludec (input  logic [10:0] funct,  
-					input  logic [1:0]  aluop,  
-					output logic [3:0] alucontrol);  				
-						
+module aludec (
+		input  logic [10:0] funct,  
+		input  logic [1:0]  aluop,  
+		output logic [3:0] alucontrol
+	);
+
 	always_comb
-		if (aluop == 2'b00) alucontrol = 4'b0010;			// LDUR or STUR		
-		else if (aluop == 2'b01) alucontrol = 4'b0111; 	//CBZ
-		else if((aluop == 2'b10)  & (funct == 11'b10001011000)) alucontrol = 4'b0010;	//ADD
-		else if((aluop == 2'b10)  & (funct == 11'b11001011000)) alucontrol = 4'b0110;	//SUB
-		else if((aluop == 2'b10)  & (funct == 11'b10001010000)) alucontrol = 4'b0000;	//AND
-		else if((aluop == 2'b10)  & (funct == 11'b10101010000)) alucontrol = 4'b0001;	//OR    
-		else alucontrol = 4'b0000;
+		if (aluop == 2'b00) // LDUR or STUR
+			alucontrol = 4'b0010;
+		else if (aluop == 2'b01) // CBZ
+			alucontrol = 4'b0111; 
+		else if((aluop == 2'b10) & (funct == 11'b10001011000)) // ADD
+			alucontrol = 4'b0010;
+		else if((aluop == 2'b10) & (funct == 11'b11001011000)) // SUB
+			alucontrol = 4'b0110;
+		else if((aluop == 2'b10) & (funct == 11'b10001010000)) // AND
+			alucontrol = 4'b0000;
+		else if((aluop == 2'b10) & (funct == 11'b10101010000)) // OR
+			alucontrol = 4'b0001;
+		else
+			alucontrol = 4'b0000;
 endmodule
